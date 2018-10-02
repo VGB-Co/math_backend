388a2442929622f8c1775fdb9095c72334ed486c
Fyccek <ficcek13@gmail.com>
2018-10-02 16:36:37 +0200

    init from me

--- rft_webapp/wsgi.py
+++ rft_webapp/wsgi.py
@@ -5,6 +5,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+haha
 """
 
 import os
