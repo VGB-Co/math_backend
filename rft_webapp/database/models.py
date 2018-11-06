dedccfdc3816e179f9112e6a26a4a46f691ac681
Fyccek <ficcek13@gmail.com>
2018-11-06 11:23:14 +0100

    table created

--- rft_webapp/database/models.py
+++ rft_webapp/database/models.py
@@ -10,8 +10,8 @@ class User(models.Model):
     user_id = models.CharField(max_length=28)
 
 
-class Results(models.Model):
+'''class Results(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.Model(Type)
-    time = models.Model(datetime.timedelta)
+    time = models.Model(datetime.timedelta)'''
 
--- rft_webapp/settings.py
+++ rft_webapp/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+#import django
+#django.setup()
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -38,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'database.models'
+    'rft_webapp.database'
 ]
 
 MIDDLEWARE = [
