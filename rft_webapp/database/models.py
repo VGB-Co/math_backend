d5636838c64a9b4fb7c4052e73a41e0d8e5c312a
Fyccek <ficcek13@gmail.com>
2018-11-13 09:22:50 +0100

    queries under maintenance

--- rft_webapp/database/models.py
+++ rft_webapp/database/models.py
@@ -5,8 +5,7 @@ from django.db import models
 
 
 class User(models.Model):
-    user_id = models.CharField(max_length=28)
-    user_name = models.CharField(max_length=10)
+    user_id = models.CharField(max_length=10, primary_key=True, null=False, unique=True)
 
 
 class Results(models.Model):
--- rft_webapp/queries/query.py
+++ rft_webapp/queries/query.py
@@ -2,7 +2,19 @@ from rft_webapp.database.models import User, Results
 
 
 def insertuser(username):
-    user = User(user_name=username)
+    user = User(user_id=username)
+    user.save()
+
 
 def insertresults(par_userid, par_type, par_time):
-    result = Results(user_id = par_userid, type = par_type, time = par_time)
+    result = Results(user_id=par_userid, type=par_type, time=par_time)
+    result.save()
+
+
+def selectresultsbytype(par_type):
+    result = Results(type = par_type)
+
+
+def selectallresults():
+    result = Results.objects.all()
+
