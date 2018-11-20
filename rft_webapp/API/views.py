951208bc332741af6d00a6cb67ec9ba282bc27aa
Fyccek <ficcek13@gmail.com>
2018-11-20 20:56:16 +0100

    get request started

--- rft_webapp/API/views.py
+++ rft_webapp/API/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+#import jwt
+
 
 # Create your views here.
+@api_view(['GET'])
+def taskreq(request):
+    r = request.META.get('HTTP_AUTHENTICATION')
+    return Response("Hello Világ")
