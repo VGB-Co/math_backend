c57abf7034ade0bf8b3b8c76aafc0dfc3c259b63
Fyccek <ficcek13@gmail.com>
2018-11-21 09:39:57 +0100

    no module named requests & jwt

--- rft_webapp/API/views.py
+++ rft_webapp/API/views.py
@@ -3,11 +3,15 @@ from __future__ import unicode_literals
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-#import jwt
+import requests
 
 
 # Create your views here.
-@api_view(['GET'])
+'''@api_view(['GET'])
 def taskreq(request):
-    r = request.META.get('HTTP_AUTHENTICATION')
-    return Response("Hello Világ")
+    encoded_user = jwt.encode({'user_id': "abcd"}, 'SECRET', algorithm='HS256')
+    return Response(encoded_user)'''
+@api_view(['GET'])
+def sds(request):
+    r = requests.get('https://api.github.com/events')
+    return Response(r)
