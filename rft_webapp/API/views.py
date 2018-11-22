cf8ad16549977151115e4cb172930fdc4ba54269
Fyccek <ficcek13@gmail.com>
2018-11-22 12:54:26 +0100

    cannit find reference encode

--- rft_webapp/API/views.py
+++ rft_webapp/API/views.py
@@ -3,15 +3,11 @@ from __future__ import unicode_literals
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import requests
+import jwt
 
 
 # Create your views here.
-'''@api_view(['GET'])
+@api_view(['GET'])
 def taskreq(request):
     encoded_user = jwt.encode({'user_id': "abcd"}, 'SECRET', algorithm='HS256')
-    return Response(encoded_user)'''
-@api_view(['GET'])
-def sds(request):
-    r = requests.get('https://api.github.com/events')
-    return Response(r)
+    return Response(encoded_user)
