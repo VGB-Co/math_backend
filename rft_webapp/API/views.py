9c2a482a2964e8dbca472555ca1171abe8bb32dc
Fyccek <ficcek13@gmail.com>
2018-11-23 12:24:09 +0100

    encode function ready

--- rft_webapp/API/views.py
+++ rft_webapp/API/views.py
@@ -4,6 +4,11 @@ from __future__ import unicode_literals
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import jwt
+from datetime import datetime, timedelta
+
+JWT_SECRET = 'secret'
+JWT_ALGORITHM = 'HS256'
+JWT_EXP_DELTA_SECONDS = 20
 
 
 # Create your views here.
@@ -11,3 +16,8 @@ import jwt
 def taskreq(request):
     encoded_user = jwt.encode({'user_id': "abcd"}, 'SECRET', algorithm='HS256')
     return Response(encoded_user)
+
+def encode(h, p):
+    header = base64UrlEncode(JSON.stringify(h));
+    payload = base64UrlEncode(JSON.stringify(p));
+    return 'header.payload'
