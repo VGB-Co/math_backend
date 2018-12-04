5c855f4333622f261feba94147305fb2a7415c80
Fyccek <ficcek13@gmail.com>
2018-12-04 10:22:05 +0100

    reformat

--- rft_webapp/API/views.py
+++ rft_webapp/API/views.py
@@ -53,9 +53,6 @@ def register(request):
     if username is None or password is None or email is None:
         return Response({'error': 'Please provide both username and password'},
                         status=HTTP_400_BAD_REQUEST)
-    if User.objects.get(username=username).pk:
-        return Response({'error': 'Username is already use'},
-                        status=HTTP_400_BAD_REQUEST)
     user = User.objects.create_user(username=username, email=email, password=password)
     token, _ = Token.objects.get_or_create(user=user)
     return Response({'token': token.key, 'name': user.username, 'email': user.email},
