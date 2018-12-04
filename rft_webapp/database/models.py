b662e2895cb5768080da34204206660b2a01d39e
Fyccek <ficcek13@gmail.com>
2018-12-04 14:25:10 +0100

    foreign key OK

--- rft_webapp/database/models.py
+++ rft_webapp/database/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 
 class Results(models.Model):
-    user = models.CharField(max_length=30)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     type = models.IntegerField()
-    time = models.FloatField()
+    time = models.FloatField() 
